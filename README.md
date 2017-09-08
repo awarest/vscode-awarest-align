b15e9abfbbeb1aecb140c9677a3394a4ebcf538d
Brian Jorden <brian.jorden@awaresolutions.com>
2017-09-08 12:34:52 -0400

    added new linebreak functionality, small refactor to whitespace, updated readme with notes/contact info

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ Heavily inspired by a method of whitespace alignment in Sublime Text 3 when usin
 Also referenced and learned from this VS Code extension for alignment (https://github.com/annsk/vscode-alignment)
 
 
+NOTES - I'm working in a Linux development environment, the linebreaks may not work perfectly for Mac/Windows at the moment. Same with potential hotkey conflicts. If anyone is ACTUALLY interested in or trying to use this, I'll tweak/fix some of that stuff too. Ping me on twitter at @brian_jorden or throw an issue on Github to let me know, otherwise, this is just more convienent for me to publish/pull down for myself.
+
+
+UPDATE: Added a new bit of functionality to handle linebreaks. The current hotkey defaults to `meta+enter` and the "selection" works the same as the whitespace handler description below. If you have a single line selected, it will split that line into new separate lines based on de-duped spaces/tabs (one new line for any sequence of spaces/tabs). If you have more than one line selected, it will combine them into a single line with any spaces/tabs/linebreaks de-duped. In either situation, the indentation of the first line selected is re-used for the one or all lines.
+
+
 Current version assumes a hotkey of `meta+space` that works on whole lines of text. What that means is if your cursor is on a single line, it runs for just that line. If you have a couple lines selected, but the cursor is at the very start of a line, it doesn't consider that last line.
 
 When aligning the following steps/logic/pattern is followed:
--- src/extension.ts
+++ src/extension.ts
@@ -3,24 +3,39 @@ import * as vscode from 'vscode';
 
 class Align {
 
-  basic () {
+  linebreaks () {
     const editor = vscode.window.activeTextEditor;
     if (!editor) { return; }
+
+    let range = this.getRange(editor.selections[0]);
+    let text = editor.document.getText(range);
+    let indent = ' '.repeat(this.getIndent(text));
+    let { start, end } = range;
+
+    // do our cleanup that needs to happen either way
+    text = this.getClean(text);
+
+    // if the start/end is the same line, or if they don't have any characters
+    // selected on the following line, we know they are trying to split a line
+    if (start.line == end.line || (end.character == 0 && (start.line + 1) == end.line)) {
+      text = this.getClean(text).split(/\s/).join('\n' + indent);
+    }
+
+    // time to actually write the text back to the editor
+    // we need to add the indent back to the first line
+    // and make sure we replace the last linebreak that got trimmed
+    editor.edit((editBuilder) => {
+      editBuilder.replace(range, indent + text + '\n');
+    });
   }
+
 
   whitespace () {
     const editor = vscode.window.activeTextEditor;
     if (!editor) { return; }
 
     editor.selections.forEach((selection) => {
-      // if we have anything selelected on the final line, make the end be the beginning
-      // of the line after the last real thing we had selected
-      let endLine = selection.end.line + ((selection.end.character > 0) ? 1 : 0);
-      if (endLine == selection.start.line) { endLine = endLine + 1; }
-
-      // selection's range is the start of any line we have something selected on
-      // all the way through to the start of the line after anything "real" selected
-      let range = new vscode.Range(selection.start.line, 0, endLine, 0);
+      let range = this.getRange(selection);
 
       let lines:   string[]   = []; // the finished up lines of text before updating the editor
       let indent:  number     = 0;  // maximum indentation of the selection for use on each line
@@ -29,12 +44,8 @@ class Align {
 
       // loop over each line, cleaning up the actual line and splitting into words
       editor.document.getText(range).split(/\r\n|\r|\n/).forEach((line) => {
-        // look for the largest indent so we can replace it later
-        indent = Math.max(0, indent, line.search(/\S/));
-        // trim leading/trailing spaces and de-dupe any repeating spaces
-        line = line.trim().replace(/\s{2,}/g, ' ');
-        // further split our words for each line
-        words.push(line.split(/\s/));
+        indent = this.getIndent(line, indent);
+        words.push(this.getClean(line).split(/\s/));
       });
 
       // loops on our temporary "lines" and captures the max
@@ -70,6 +81,32 @@ class Align {
   }
 
 
+
+
+  getRange ({ start, end }: vscode.Selection): vscode.Range {
+    // if we have anything selelected on the final line, make the end be the
+    // beginning of the line after the last real thing we had selected
+    let endLine = end.line + ((end.character > 0) ? 1 : 0);
+    if (endLine == start.line) { endLine = endLine + 1; }
+
+    // selection's range is the start of any line we have something selected on
+    // all the way through to the start of the line after anything "real" selected
+    return new vscode.Range(start.line, 0, endLine, 0);
+  }
+
+  getIndent (text: string = '', max: number = 0): number {
+    // look for the largest indent so we can replace it later
+    // based on the optional previous max or zero at minimum
+    return Math.max(0, max, text.search(/\S/));
+  }
+
+  getClean (text: string = ''): string {
+    // replace any series of spaces/tabs/linebreaks with a single space and trim
+    return text.replace(/[\s]+/g, ' ').trim();
+  }
+
+
+
 }
 
 
@@ -78,8 +115,8 @@ export function activate (context: vscode.ExtensionContext) {
   const align = new Align();
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('extension.awarestAlignBasic', () => {
-      align.basic();
+    vscode.commands.registerCommand('extension.awarestAlignLinebreaks', () => {
+      align.linebreaks();
     })
   );
 
